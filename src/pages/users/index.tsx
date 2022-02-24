1eaccfb90fec376cb5f005ac40dee32e438c1558
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-02-24 19:13:19 -0300

    Controle de usuário
    Criada a página de lista de usuário, página de form de usuário e componente de paginação.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,8 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList(){
@@ -8,6 +11,108 @@ export default function UserList(){
             <Header />
             <Flex w='100%' my='6' maxWidth={1400} mx='auto' px='6'>
                 <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+                        <Button 
+                            as="a" 
+                            size="sm" 
+                            fontSize="sm" 
+                            colorScheme="pink" 
+                            leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+                        >
+                            Criar novo
+                        </Button>
+                    </Flex>
+
+                    <Table colorScheme='whiteAlpha'>
+                        <Thead>
+                            <Tr>
+                                <Th px='6' color='gray.300' width='8' >
+                                    <Checkbox colorScheme='pink'/>
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th width='8'></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px='6'>
+                                    <Checkbox colorScheme='pink'/>
+                                </Td> 
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'>Keven Reis</Text>
+                                        <Text fontSize='sm' color='gray.300'>kevenlord@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>01 de Julho, 2019</Td>
+                                <Td>
+                                    <Button 
+                                        as="a" 
+                                        size="sm" 
+                                        fontSize="sm" 
+                                        colorScheme="purple" 
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                                    >
+                                        Editar
+                                </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px='6'>
+                                    <Checkbox colorScheme='pink'/>
+                                </Td> 
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'>Keven Reis</Text>
+                                        <Text fontSize='sm' color='gray.300'>kevenlord@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>01 de Julho, 2019</Td>
+                                <Td>
+                                    <Button 
+                                        as="a" 
+                                        size="sm" 
+                                        fontSize="sm" 
+                                        colorScheme="purple" 
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                                    >
+                                        Editar
+                                </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px='6'>
+                                    <Checkbox colorScheme='pink'/>
+                                </Td> 
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'>Keven Reis</Text>
+                                        <Text fontSize='sm' color='gray.300'>kevenlord@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>01 de Julho, 2019</Td>
+                                <Td>
+                                    <Button 
+                                        as="a" 
+                                        size="sm" 
+                                        fontSize="sm" 
+                                        colorScheme="purple" 
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                                    >
+                                        Editar
+                                </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                    
+                    <Pagination/>
+                </Box>
             </Flex>
         </Box>
     );

41f679c5d93e2acfd0c7f69742cf694b60603aa4
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-03-04 18:31:51 -0300

    Axios
    Fora adicionado Axios no projecto

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,15 +7,16 @@ import { useQuery } from 'react-query'
 import { Header } from "../../components/Header/index";
 import { Pagination } from "../../components/Pagination/index";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
+    const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+        const { data } = await api.get('users')
+        
 
 
-        const users = data.users.map(user =>{
-            return{
+        const users = data.users.map(user => {
+            return {
                 id: user.id,
                 name: user.name,
                 email: user.email,
@@ -28,6 +29,8 @@ export default function UserList() {
         })
 
         return users;
+    }, {
+        staleTime: 1000 * 5 // 5 seg
     })
 
 
@@ -45,7 +48,11 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+
+                            {!isLoading && isFetching &&  <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
 
                         <Link href="/users/create" passHref>

3535b98c911d2d9ae22d03c26c63b4d6f9baa02b
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-03-04 16:10:41 -0300

    Faker
    Fora adicionado faker ao projeto, porém devido a problemas de sua ultima versão, foi feita o downgrade para 5.5.3

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker'
 
 type User = {
     name: string;
@@ -7,13 +8,32 @@ type User = {
 }
 
 
-export function makeServer(){
+export function makeServer() {
     const server = createServer({
         models: {
             user: Model.extend<Partial<User>>({})
         },
 
-        routes(){
+        factories: {
+            user: Factory.extend({
+                name(i: number) { 
+                    return `User ${i + 1}`
+                },
+                email() { 
+                    return faker.internet.email().toLocaleLowerCase();
+                },
+                createdAt() { 
+                    return faker.date.recent(10);
+                },
+
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 200)
+        },
+
+        routes() {
             this.namespace = 'api';
             this.timing = 750;
 

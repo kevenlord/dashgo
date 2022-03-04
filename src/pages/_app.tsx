cc03a4b62e67c27edb79fbcc0f3543f92a8b661b
Keven Reis <39449222+kevenlord@users.noreply.github.com>
2022-03-04 18:09:04 -0300

    Listagem
    Agora os usuário estão sendo listados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,28 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContent'
 import { makeServer } from '../services/mirage'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
 
-    </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,21 +1,41 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
+import { useQuery } from 'react-query'
+
 
 import { Header } from "../../components/Header/index";
 import { Pagination } from "../../components/Pagination/index";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+
+        const users = data.users.map(user =>{
+            return{
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users;
+    })
+
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
 
-    useEffect(()=>{
-        fetch('http://localhost:3000/api/users').then(response => response.json()).then(data => console.log(data))
-    },[])
 
     return (
         <Box>
@@ -41,63 +61,52 @@ export default function UserList() {
                         </Link>
 
                     </Flex>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter os dados dos usários</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme='whiteAlpha'>
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color='gray.300' width='8' >
+                                            <Checkbox colorScheme='pink' />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
 
-                    <Table colorScheme='whiteAlpha'>
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color='gray.300' width='8' >
-                                    <Checkbox colorScheme='pink' />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width='8'></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight='bold'>{user.name}</Text>
+                                                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
 
-                                <Th width='8'></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Keven Reis</Text>
-                                        <Text fontSize='sm' color='gray.300'>kevenlord@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>01 de Julho, 2019</Td>}
+                            <Pagination />
+                        </>
+                    )}
 
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Keven Reis</Text>
-                                        <Text fontSize='sm' color='gray.300'>kevenlord@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>03 de Julho, 2019</Td>}
-
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Keven Reis</Text>
-                                        <Text fontSize='sm' color='gray.300'>kevenlord@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>05 de Julho, 2019</Td>}
-
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination />
                 </Box>
             </Flex>
         </Box>
